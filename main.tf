be7b4232fc09a1f18d92db96e3c1c524bf243631
Anton Whalley <antonwhalley@yahoo.com>
2023-12-23 00:32:06 +0000

    fix: remove provider tag completely

--- main.tf
+++ main.tf
@@ -1,12 +1,3 @@
-terraform {
-  required_providers {
-    ibm = {
-      source  = "IBM-Cloud/ibm"
-      version = "~>1.56.1"
-    }
-  }
-  required_version = ">=1.3"
-}
 
 resource null_resource print_values {
   provisioner "local-exec" {
